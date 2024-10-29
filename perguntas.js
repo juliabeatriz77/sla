13ae88487fcfc6c686ae0cc77b4682cc63626dff
juliabeatriz77 <julia.ignacio.santos@escola.pr.gov.br>
2024-10-29 17:17:56 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Conhecimentos gerais',
+    'Quem inventou a lâmpada?',
+    'Thomas Edison'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Quantos ossos temos no corpo?',
+    '206'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Qual o maior planeta do sistema solar?',
+    'Júpiter'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Qual o planeta mais próximo do sol?',
+    'Mercúrio'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Quantos continentes existem?',
+    '6'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'quas as duas línguas mais faladas no mundo?',
+    'inglês e mandarim chinês'
+)
